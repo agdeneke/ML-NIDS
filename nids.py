2e1a6f958e0986eb6231360038909ff09097d44e
Abiel Deneke <abiel.deneke@gmail.com>
2025-09-05 05:51:45 -0500

    Use Kitsune dataset with labels as training data

--- nids.py
+++ nids.py
@@ -2,12 +2,13 @@ import torch
 import pandas as pd
 
 class PacketDataset(torch.utils.data.Dataset):
-    def __init__(self, packet_capture_file):
-        self.packets = pd.read_csv(packet_capture_file)
+    def __init__(self, packet_capture_file, labels_file, max_number_of_packets):
+        self.packets = pd.read_csv(packet_capture_file, nrows=max_number_of_packets)
+        self.labels = pd.read_csv(labels_file, nrows=max_number_of_packets)
     def __len__(self):
         return len(self.packets)
     def __getitem__(self, idx):
-        return self.packets.iloc[idx]
+        return pd.concat([self.packets.iloc[idx], self.labels.iloc[idx]])
 
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
 print(f"Using {device} device")
--- test.py
+++ test.py
@@ -3,28 +3,36 @@ import nids
 
 class PacketDatasetTest(unittest.TestCase):
     def setUp(self):
-        self.packet_dataset = nids.PacketDataset("out.csv")
+        max_number_of_packets = 100
+        self.packet_dataset = nids.PacketDataset("out.csv", "labels.csv", max_number_of_packets)
 
     def test_get_number_of_packets(self):
-        number_of_packets = 38
+        max_number_of_packets = 100
 
         number_of_packets_result = len(self.packet_dataset)
 
-        self.assertEqual(number_of_packets_result, number_of_packets)
+        self.assertEqual(number_of_packets_result, max_number_of_packets)
 
     def test_get_packet_length(self):
-        packet_length = 175
+        packet_length = 1294
 
-        packet_length_result = self.packet_dataset[3]["length"]
+        packet_length_result = self.packet_dataset[0]["Length"]
 
         self.assertEqual(packet_length_result, packet_length)
 
-    def test_get_packet_destination_mac(self):
-        destination_mac = "ff:ff:ff:ff:ff:ff"
+    def test_get_packet_destination(self):
+        destination = "192.168.100.5"
 
-        destination_mac_result = self.packet_dataset[0]["destination_mac"]
+        destination_result = self.packet_dataset[0]["Destination"]
 
-        self.assertEqual(destination_mac_result, destination_mac)
+        self.assertEqual(destination_result, destination)
+
+    def test_get_packet_label(self):
+        label = 0
+
+        label_result = self.packet_dataset[0]["x"]
+
+        self.assertEqual(label_result, label)
 
 if __name__ == '__main__':
     unittest.main()
