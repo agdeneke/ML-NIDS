60647d4715dadfbff1288a61cb9f72979d730a99
Abiel Deneke <abiel.deneke@gmail.com>
2025-09-07 02:41:27 -0500

    Remove unnecessary graphics card setup

--- nids.py
+++ nids.py
@@ -16,6 +16,3 @@ class NeuralNetwork(torch.nn.Module):
 
     def forward(self, input):
         return input
-
-device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
-print(f"Using {device} device")
