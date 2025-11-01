18f8309c986cd23556cffacb125f045e26e4110f
Abiel Deneke <abiel.deneke@gmail.com>
2025-11-01 04:51:53 -0500

    Refactor main.py and listen for network traffic

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import nids
 import torch
 import sys
+import scapy
+import scapy.sendrecv
 
 def train_loop(dataloader, model, loss_fn, optimizer):
     model.train()
@@ -37,50 +39,53 @@ def test_loop(dataloader, model):
 
     print(f"Accuracy: {(correct / size) * 100}%")
 
-if len(sys.argv) > 4 or sys.argv[1] not in ["--train", "--test"]:
-    print("Usage:")
-    print("")
-    print("main.py <--train/test> <training_or_test_dataset.csv> <training_or_test_labels.csv>")
-    sys.exit(1)
-
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
 print(f"Using {device} device")
 
-packet_capture_filename = sys.argv[2]
-labels_filename = sys.argv[3]
-packet_df = pd.read_csv(packet_capture_filename)
-label_df = pd.read_csv(labels_filename)
-
-features_to_drop = ["No.", "Time", "Info"]
-packet_df = packet_df.drop(features_to_drop, axis="columns")
-
-packet_df = pd.get_dummies(packet_df, columns=["Protocol", "Source", "Destination"], dtype=float)
-
-packet_dataset = nids.PacketDataset(packet_df, label_df)
-
 model = nids.NeuralNetwork(input_features=29, output_features=2).to(device)
 
-if sys.argv[1] == "--train":
-    training_packet_dataset, validation_packet_dataset = torch.utils.data.random_split(packet_dataset, [0.5, 0.5])
-    training_dataloader = torch.utils.data.DataLoader(training_packet_dataset, batch_size=64)
-    validation_dataloader = torch.utils.data.DataLoader(validation_packet_dataset, batch_size=64, drop_last=True)
+if len(sys.argv) > 1:
+    if sys.argv[1] not in ["--train", "--test"]:
+        print("Usage:")
+        print("")
+        print("main.py <--train/test> <training_or_test_dataset.csv> <training_or_test_labels.csv>")
+        sys.exit(1)
 
-    loss_fn = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
+    packet_capture_filename = sys.argv[2]
+    labels_filename = sys.argv[3]
+    packet_df = pd.read_csv(packet_capture_filename)
+    label_df = pd.read_csv(labels_filename)
 
-    epoch = 1
-    for i in range(0, epoch):
-        train_loop(training_dataloader, model, loss_fn, optimizer)
-        test_loop(validation_dataloader, model)
+    features_to_drop = ["No.", "Time", "Info"]
+    packet_df = packet_df.drop(features_to_drop, axis="columns")
 
-    torch.save(model.state_dict(), 'model_weights.pth')
-elif sys.argv[1] == "--test":
-    test_dataloader = torch.utils.data.DataLoader(packet_dataset, batch_size=64)
+    packet_df = pd.get_dummies(packet_df, columns=["Protocol", "Source", "Destination"], dtype=float)
 
-    try:
-        model.load_state_dict(torch.load('model_weights.pth', weights_only=True))
-        model.eval()
+    packet_dataset = nids.PacketDataset(packet_df, label_df)
 
-        test_loop(test_dataloader, model)
-    except FileNotFoundError:
-        print("Error: Model not found.")
+    if sys.argv[1] == "--train":
+        training_packet_dataset, validation_packet_dataset = torch.utils.data.random_split(packet_dataset, [0.5, 0.5])
+        training_dataloader = torch.utils.data.DataLoader(training_packet_dataset, batch_size=64)
+        validation_dataloader = torch.utils.data.DataLoader(validation_packet_dataset, batch_size=64, drop_last=True)
+
+        loss_fn = torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
+
+        epoch = 1
+        for i in range(0, epoch):
+            train_loop(training_dataloader, model, loss_fn, optimizer)
+            test_loop(validation_dataloader, model)
+
+        torch.save(model.state_dict(), 'model_weights.pth')
+    elif sys.argv[1] == "--test":
+        test_dataloader = torch.utils.data.DataLoader(packet_dataset, batch_size=64)
+
+        try:
+            model.load_state_dict(torch.load('model_weights.pth', weights_only=True))
+            model.eval()
+
+            test_loop(test_dataloader, model)
+        except FileNotFoundError:
+            print("Error: Model not found.")
+else:
+    print(scapy.sendrecv.sniff(prn=scapy.packet.Packet.summary))
