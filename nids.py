867c3f36001f0450948e3e3cda008ebeb34429e7
Abiel Deneke <abiel.deneke@gmail.com>
2026-05-10 16:37:49 -0500

    Fix chained assignment error and remove single-layer linear model

--- nids.py
+++ nids.py
@@ -8,13 +8,15 @@ class PacketDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.packets)
     def __getitem__(self, idx):
-        return self.packets.iloc[idx].to_numpy(), self.labels.loc[idx, "x"]
+        packet = self.packets.iloc[idx].to_numpy()
+        packet.setflags(write=True)
+
+        return packet, self.labels.loc[idx, "x"]
 
 class NeuralNetwork(torch.nn.Module):
     def __init__(self, input_features, output_features):
         super().__init__()
 
-        self.linear = torch.nn.Linear(input_features, output_features)
         self.linear_relu_stack = torch.nn.Sequential(torch.nn.Linear(input_features, 20),
                                                      torch.nn.ReLU(),
                                                      torch.nn.Linear(20, output_features))
