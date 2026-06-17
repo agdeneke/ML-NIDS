8a34e1ce18e77840a4131467a3bb863f55dafca0
Abiel Deneke <abiel.deneke@gmail.com>
2026-06-16 21:40:09 -0500

    Implement CORS to allow frontend to access the backend server

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import sniffer
 import pandas as pd
 from flask import Flask
+from flask_cors import CORS
 
 
 def create_app(packet_sniffer: sniffer.PacketSniffer):
     app = Flask(__name__)
+    CORS(app)
 
     @app.route("/api/attacks")
     def attack_packets():
